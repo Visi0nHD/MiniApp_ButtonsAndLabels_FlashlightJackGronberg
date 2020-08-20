317aac9de3567ed6e58879964d5c9277f3c038ba
Jack Gronberg <Jackgronberg@gmail.com>
2020-08-19 23:07:10 -0500

    Initial Commit

---
+++ MiniApp_ButtonsAndLabels_FlashlightJackGronberg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniApp_ButtonsAndLabels_FlashlightJackGronberg
+//
+//  Created by Tiger Coder on 8/19/20.
+//  Copyright © 2020 Jack Gronberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
