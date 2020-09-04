3f799f59173c5d943e95f7ec991ad8a2360e8a3e
Jack Gronberg <Jackgronberg@gmail.com>
2020-09-04 14:32:24 -0500

    made first comment

--- MiniApp_ButtonsAndLabels_FlashlightJackGronberg/ViewController.swift
+++ MiniApp_ButtonsAndLabels_FlashlightJackGronberg/ViewController.swift
@@ -1,20 +1,45 @@
-//
-//  ViewController.swift
-//  MiniApp_ButtonsAndLabels_FlashlightJackGronberg
-//
-//  Created by Tiger Coder on 8/19/20.
-//  Copyright © 2020 Jack Gronberg. All rights reserved.
-//
+    //
+    //  ViewController.swift
+    //  MiniApp_ButtonsAndLabels_FlashlightJackGronberg
+    //
+    //  Created by Tiger Coder on 8/19/20.
+    //  Copyright © 2020 Jack Gronberg. All rights reserved.
+    //
 
-import UIKit
+    import UIKit
 
-class ViewController: UIViewController {
+    class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //first comment wow
+        @IBOutlet weak var OffButton: UIButton!
+        @IBOutlet weak var OnButton: UIButton!
+        
+        @IBOutlet weak var LabelSwap: UILabel!
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+            
+            
+           
+            
+        }
+
+        @IBAction func OnButtonAction(_ sender: Any) {
+         view.backgroundColor = UIColor.white
+            LabelSwap.text = "ON"
+            OnButton.backgroundColor = UIColor.white
+            OnButton.setTitleColor(UIColor.white, for: .normal)
+            OffButton.setTitleColor(UIColor.red, for: .normal)
+        }
+        
+        @IBAction func OffButtonAction(_ sender: Any) {
+            view.backgroundColor = UIColor.black
+            LabelSwap.text = "OFF"
+            OffButton.backgroundColor = UIColor.black
+            OffButton.setTitleColor(UIColor.black, for: .normal)
+            OnButton.setTitleColor(UIColor.blue, for: .normal)
+        }
     }
-
-
-}
 
